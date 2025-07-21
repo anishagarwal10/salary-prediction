7b3f29d031943ae830567edec7bde991892debd9
anishagarwal10 <anesh.agarwal98998@gmail.com>
2025-07-21 12:38:19 +0530

    updated all files

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import pickle
 import numpy as np
 
 app = Flask(__name__)
-
-# Load the trained model
 model = pickle.load(open('salary_predictor_model.pkl', 'rb'))
 
 @app.route('/')
@@ -13,13 +11,14 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        features = [float(x) for x in request.form.values()]
-        final_features = np.array(features).reshape(1, -1)
-        prediction = model.predict(final_features)[0]
-        return render_template('index.html', prediction_text=f'Predicted Salary: ₹{prediction:.2f}')
-    except Exception as e:
-        return render_template('index.html', prediction_text='Error in prediction: ' + str(e))
+    if request.method == 'POST':
+        try:
+            experience = float(request.form['experience'])
+            prediction = model.predict(np.array([[experience]]))
+            return render_template('index.html', prediction_text=f'Predicted Salary: ₹{prediction[0]:,.2f}')
+        except Exception as e:
+            return render_template('index.html', prediction_text=f'Error: {e}')
+    return "Method Not Allowed", 405
 
 if __name__ == '__main__':
     app.run(debug=True)
